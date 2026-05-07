2de8b00d296633bf6e26fc32cbbbc35cec81bebf
Alejandra Castro <TU_EMAIL@gmail.com>
2026-05-07 19:26:19 -0300

    chore: bump 1.1.3 (14) y projectId

--- app.config.js
+++ app.config.js
@@ -28,11 +28,11 @@ module.exports = ({ config } = {}) => {
   const hasGoogleSigninPlugin = nextPlugins.some((p) => Array.isArray(p) && p[0] === '@react-native-google-signin/google-signin');
   return {
     ...incoming,
-    version: '1.1.2',
+    version: '1.1.3',
     ios: { ...(incoming.ios || {}) },
     android: {
       ...(incoming.android || {}),
-      versionCode: 13,
+      versionCode: 14,
       package: 'com.u.lumina',
     },
     web: { ...(incoming.web || {}) },
@@ -50,7 +50,7 @@ module.exports = ({ config } = {}) => {
         ],
     extra: {
       ...(incoming.extra || {}),
-      eas: { ...((incoming.extra && incoming.extra.eas) || {}), projectId: 'e11902b4-a0c0-4318-9bc7-8cfb1c52b8b9' },
+      eas: { ...((incoming.extra && incoming.extra.eas) || {}), projectId: 'e11982b4-a0c0-4318-9bc7-8cfb1c52b8b9' },
       vapidPublicKey: process.env.EXPO_PUBLIC_VAPID_PUBLIC_KEY,
       googleAndroidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
       googleWebClientId: process.env.GOOGLE_CLIENT_ID || process.env.EXPO_PUBLIC_GOOGLE_WEB_CLIENT_ID,
